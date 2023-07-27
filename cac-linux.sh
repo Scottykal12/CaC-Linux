795773503e4e442d6875c443c3d127de983c136a
Scott Kalinec <scottykal12@gmail.com>
2023-07-27 09:55:43 +0200

    init

---
+++ cac-linux.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# coolkey is what cyber.mil sugests
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install firefox -y
+sudo apt install coolkey -y
+
+mkdir $HOME/.mozilla/certificates
+mkdir $HOME/tmp
+
+cd $HOME/tmp
+
+wget https://dl.dod.cyber.mil/wp-content/uploads/pki-pke/zip/unclass-certificates_pkcs7_v5-6_dod.zip
+unzip unclass-certificates_pkcs7_v5-6_dod.zip
+
+# need to get location of .so file
+libloc=$(sudo find / -name "libcoolkeypk11.so" 2>/dev/null | head -n 1)
+certfolder=/$HOME/.mozilla/certificates/
+
+certutil -d $certfolder -N
+modutil -dbdir sql:$certfolder -add "CAC Module" -libfile $libloc
+
+for i in $(find Certificates_PKCS7_v5.6_DoD/ -name "*.p7b"); do
+    certutil -A -n $i -t C,C,C -d $certfolder -a -i $i
+    done
+
+rm -rf $HOME/tmp/
+
+# /usr/lib/mozilla/certificates
+# /usr/lib64/mozilla/certificates
+# ~/.mozilla/certificates
