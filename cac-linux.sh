c53946e751175b953ace87ec9a1608fc5a7c5567
Scott Kalinec <scottykal12@gmail.com>
2023-07-28 08:39:47 +0200

    openssl conert to cer

--- cac-linux.sh
+++ cac-linux.sh
@@ -7,9 +7,11 @@ sudo apt upgrade -y
 sudo apt install firefox -y
 sudo apt install coolkey -y
 sudo apt install pcscd -y
+sudo apt install openssl -y
 
 mkdir $HOME/.mozilla/certificates
 mkdir $HOME/tmp
+mkdir $HOME/tmp/rootCAcer
 
 cd $HOME/tmp
 
@@ -19,19 +21,20 @@ unzip -o unclass-certificates_pkcs7_v5-6_dod.zip
 # need to get location of .so file
 libloc=$(sudo find / -name "libcoolkeypk11.so" 2>/dev/null | head -n 1)
 certfolder=$HOME/.mozilla/certificates/
+rootCAcer=$HOME/tmp/rootCAcer
 
 echo $libloc
 
 certutil -d $certfolder -N --empty-password
 modutil -dbdir sql:$certfolder -add "CAC Module" -libfile $libloc
 
-for i in $(find $PWD/Certificates_PKCS7_v5.6_DoD/ -name "*.p7b"); do
-    certutil -A -n $i -t TC,C,T -d $certfolder -a -i $i
-    echo $i
-    echo "end"
+for i in $(find $PWD/Certificates_PKCS7_v5.6_DoD/ -name "*Root_CA*"); do
+    name=$(basename $i .der.p7b)
+    openssl pkcs7 -print_certs -inform DER -in $i -out $name.cer
+    certutil -A -n $name -t TC,C,T -d $certfolder -a -i $name.cer
     done
 
-rm -rf $HOME/tmp/
+# rm -rf $HOME/tmp/
 
 # /usr/lib/mozilla/certificates
 # /usr/lib64/mozilla/certificates
